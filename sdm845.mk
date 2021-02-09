6b9b7fbaa8a03ad0110cda3b1dca72e551d4393f
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    Revert "sdm845-common: Switch to source built vendor.qti.hardware.perf@2.0"
    This reverts commit 2da104bd1cdf5d62e2415c600430593010981e3b.

--- sdm845.mk
+++ sdm845.mk
@@ -254,8 +254,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
